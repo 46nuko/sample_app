d7f56232edc13cd7d18a587c80778b273bc678a8
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-01-21 09:11:44 +0000

    4回目の更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :lists
+  get 'lists/new'
+  get 'lists/edit'
+  get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
-  get '/top' => 'homes#top'
-  get 'lists'=>'lists#index'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show'
+  get 'lists/:id'=>'lists#show', as: 'list'
 end
